48754ac776d869d3659d7e8176927841705de9d6
Ana Isabel Cuéllar Maestro <100219176+anaicm@users.noreply.github.com>
2023-10-07 12:19:15 +0200

    modificación para que los componentes navegen

--- club_social/src/index.js
+++ club_social/src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//---------------------------------------------------------------------------
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Actividades } from './componentes/actividades';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/Actividades",
+    element: <Actividades/>,
+  },
+ 
+]);
 
+//------------------------------------------------------------------------------
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
 
