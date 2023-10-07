acbf50c25cdad44ef86726ceace082aa34c0ee97
Ana Isabel Cuéllar Maestro <100219176+anaicm@users.noreply.github.com>
2023-10-07 12:19:38 +0200

    cambios para navegar de componente

--- club_social/src/componentes/boton.jsx
+++ club_social/src/componentes/boton.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import styles from '../css/boton.module.css';
-;
+import { Link } from "react-router-dom";//link para las rutas hacia los componentes
+
 
 export function Boton() {
   return (
     <div className={styles.contBtn}>
-      <p className={styles.button}>
-        Entrar
-      </p>
+      
+          <Link to="/Actividades" className={styles.button}>
+              Entrar
+          </Link>
+      
     </div>
   );
 }
